6adf288cb75df31b02210ef8358030cf9cd7c15f
pavan <pavan.marri@zemosolabs.com>
2020-12-18 07:38:23 +0530

    refactored DecisionTableResult.java

--- src/test/java/com.zemoso.zinteract/TaxCalculatorTest.java
+++ src/test/java/com.zemoso.zinteract/TaxCalculatorTest.java
@@ -1,50 +1,51 @@
 package com.zemoso.zinteract;
 
-import java.util.HashMap;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class TaxCalculatorTest {
 
 	@Test
 	public void testTaxCalBelow25K() {
 		String json = TaxCalculator.getRulesJson();
-		HashMap<String, String> value = new HashMap<String, String>();
+		HashMap<String, String> value = new HashMap<>();
 		value.put("investment_80c", "1000");
 		value.put("income", "249000");
-		Double tax = TaxCalculator.execute(value, json);
-		assertTrue(tax == 0);
+		double tax = TaxCalculator.execute(value, json);
+		assertEquals(0, tax, 0.0);
 
 	}
 
 	@Test
 	public void testTaxCal25KTo50K() {
 		String json = TaxCalculator.getRulesJson();
-		HashMap<String, String> value = new HashMap<String, String>();
+		HashMap<String, String> value = new HashMap<>();
 		value.put("investment_80c", "30000");
 		value.put("income", "310000");
-		Double tax = TaxCalculator.execute(value, json);
-		assertTrue(tax == 1030.20);
+		double tax = TaxCalculator.execute(value, json);
+		assertEquals(1030.20, tax, 0.0);
 	}
 
 	@Test
 	public void testTaxCal50kTo100K() {
 		String json = TaxCalculator.getRulesJson();
-		HashMap<String, String> value = new HashMap<String, String>();
+		HashMap<String, String> value = new HashMap<>();
 		value.put("investment_80c", "10000");
 		value.put("income", "1000000");
-		Double tax = TaxCalculator.execute(value, json);
-		assertTrue(tax == 126714.60);
+		double tax = TaxCalculator.execute(value, json);
+		assertEquals(126714.60, tax, 0.0);
 	}
 
 	@Test
 	public void testTaxCalAbove100K() {
 		String json = TaxCalculator.getRulesJson();
-		HashMap<String, String> value = new HashMap<String, String>();
+		HashMap<String, String> value = new HashMap<>();
 		value.put("investment_80c", "10000");
 		value.put("income", "1100000");
-		Double tax = TaxCalculator.execute(value, json);
-		assertTrue(tax == 156590.40);
+		double tax = TaxCalculator.execute(value, json);
+		assertEquals(156590.40, tax, 0.0);
 	}
-
 }
