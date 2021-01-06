477f53a808230037e4f6ef6dcf0ccab04252d158
MallipeddiRuchitha <mallipeddiruchitha@gmail.com>
2021-01-06 10:29:37 +0530

    refactored code as per comments

--- src/main/java/com/katas/kata1/SuperMarketBilling.java
+++ src/main/java/com/katas/kata1/SuperMarketBilling.java
@@ -2,56 +2,21 @@ package com.katas.kata1;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SuperMarketBilling {
-    /**
-     *
-     * @param orders items ordered
-     * @return total amount
-     */
-    public int billing(String orders) {
-        HashMap<Character, Integer> itemsCount = new HashMap<>();
-        char item;
-        int totalBill = 0;
-        for (int i = 0; i < orders.length(); i++) {
-            item = orders.charAt(i);
-            if (itemsCount.containsKey(item)) {
-                itemsCount.put(item, itemsCount.get(item) + 1);
-            } else {
-                itemsCount.put(item, 1);
-            }
-        }
-        if (itemsCount.containsKey('A')) {
-            int itemsA = itemsCount.get('A');
-            totalBill = totalBill + (itemsA / 3) * 130 + (itemsA - 3 * (itemsA / 3)) * 50;
-        }
-        if (itemsCount.containsKey('B')) {
-            int itemsB = itemsCount.get('B');
-            totalBill = totalBill + (itemsB / 2) * 45 + (itemsB - 2 * (itemsB / 2)) * 30;
-        }
-        if (itemsCount.containsKey('C')) {
-            totalBill = totalBill + itemsCount.get('C') * 20;
 
-        }
-        if (itemsCount.containsKey('D')) {
-            totalBill = totalBill + itemsCount.get('D') * 15;
-
-        }
-
-        return totalBill;
-    }
 
     /**
-     *
      * @param orders items ordered
      * @param rules  pricing rules ,rule format is singleItemCost,offerItemsNumber-offerCost
      * @return total amount
      */
     public int billingWithVaryingRules(String orders, List<String> rules) {
-        HashMap<Character, Integer> itemsCount = new HashMap<>();
+        Map<Character, Integer> itemsCount = new HashMap<>();
         char item;
         int totalBill = 0;
-        for (int index= 0; index < orders.length(); index++) {
+        for (int index = 0; index < orders.length(); index++) {
             item = orders.charAt(index);
             if (itemsCount.containsKey(item)) {
                 itemsCount.put(item, itemsCount.get(item) + 1);
--- src/test/java/com/katas/kata1/SuperMarketBillingTest.java
+++ src/test/java/com/katas/kata1/SuperMarketBillingTest.java
@@ -13,24 +13,6 @@ class SuperMarketBillingTest {
 
     private final SuperMarketBilling superMarketBilling = new SuperMarketBilling();
 
-
-    @Test
-    void testBilling() {
-        Assert.assertEquals(0, superMarketBilling.billing(""));
-        Assert.assertEquals(50, superMarketBilling.billing("A"));
-        Assert.assertEquals(80, superMarketBilling.billing("AB"));
-        Assert.assertEquals(115, superMarketBilling.billing("CDBA"));
-        Assert.assertEquals(100, superMarketBilling.billing("AA"));
-        Assert.assertEquals(130, superMarketBilling.billing("AAA"));
-        Assert.assertEquals(180, superMarketBilling.billing("AAAA"));
-        Assert.assertEquals(230, superMarketBilling.billing("AAAAA"));
-        Assert.assertEquals(260, superMarketBilling.billing("AAAAAA"));
-        Assert.assertEquals(160, superMarketBilling.billing("AAAB"));
-        Assert.assertEquals(175, superMarketBilling.billing("AAABB"));
-        Assert.assertEquals(190, superMarketBilling.billing("AAABBD"));
-        Assert.assertEquals(190, superMarketBilling.billing("DABABA"));
-    }
-
     @Test
     void testBillingWithVaryingRules() {
         List<String> rules = new ArrayList<String>();
